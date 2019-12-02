79cde2bb105d8ce61454bb832be02f1573afcfab
ogihara-ryo <ogihara.ryo.b@gmail.com>
2019-12-02 11:20:31 +0900

    Add tasks#index page

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,3 @@
+class TasksController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
 end
