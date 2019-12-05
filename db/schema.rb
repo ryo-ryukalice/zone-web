00c91d94b67a530f817ae90a900b9d3b9b4d4e70
ogihara-ryo <ogihara.ryo.b@gmail.com>
2019-12-06 02:29:40 +0900

    Add api_token to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_152346) do
+ActiveRecord::Schema.define(version: 2019_12_05_171619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_11_30_152346) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "api_token"
   end
 
   add_foreign_key "tasks", "users"
