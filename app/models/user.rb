233280511d462bcecca48f6b10f90fec0b60c948
ogihara-ryo <ogihara.ryo.b@gmail.com>
2019-12-06 02:45:01 +0900

    User has secure API token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :tasks
 
   has_secure_password
+  has_secure_token :api_token
 
   validates :name, presence: true
   validates :account_id, presence: true, uniqueness: true
